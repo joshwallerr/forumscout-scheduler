c4d089c026c5db337cd9b87ed00b89e36cede48a
Josh Waller <joshwaller101@hotmail.com>
2024-05-17 00:53:23 +0100

    hgdfhdfg

--- api/index.py
+++ api/index.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask import Flask, redirect
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import os
+
+
+
+
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get('MONGODB_URI')
+client = MongoClient(app.config["MONGO_URI"], server_api=ServerApi('1'))
+db = client.forumscout
+users = db.users
+scouts = db.scouts
+
+
+
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return redirect('https://forumscout.app', code=301)
 
-@app.route('/about')
-def about():
-    return 'About'
+
+
+@app.route('/run-scout', methods=['POST'])
+def run_scout():
+    email = request.json['email']
+    query = request.json['query']
+    
+    user = users.find_one({'email': email})
+
+
+
+
+    return "Scout ran successfully"
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
