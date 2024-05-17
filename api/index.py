f2a98e46f5939c0f5db8e048d3ccf7bdd5faa05d
Josh Waller <joshwaller101@hotmail.com>
2024-05-17 02:25:45 +0100

    fixed yaml indentation

--- api/index.py
+++ api/index.py
@@ -72,35 +72,44 @@ def create_github_action(scout):
 
     # Generate the GitHub Action workflow content
     workflow_content = f"""
-    name: Run Scout {scout['query']}
+name: Run Scout {scout['query']}
 
-    on:
-    schedule:
-        - cron:  '0 * * * *'
+on:
+  schedule:
+    - cron:  '0 * * * *'
 
-    jobs:
-    run-scout:
-        runs-on: ubuntu-latest
-        steps:
-        - name: Send POST request
-            uses: fjogeleit/http-request-action@master
-            with:
-            url: 'https://yourdomain.com/run-scout'
-            method: 'POST'
-            contentType: 'application/json'
-            data: '{{
-                "owner": "{scout['owner']}",
-                "query": "{scout['query']}",
-                "country": "{scout['country']}"
-            }}'
+jobs:
+  run-scout:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Send POST request
+        uses: fjogeleit/http-request-action@master
+        with:
+          url: 'https://yourdomain.com/run-scout'
+          method: 'POST'
+          contentType: 'application/json'
+          data: '{{
+            "owner": "{scout['owner']}",
+            "query": "{scout['query']}",
+            "country": "{scout['country']}"
+          }}'
     """
 
-    # Path where the GitHub Action will be saved
+    # Path where the GitHub Action will be saved - NEEDS TO BE CHANGED?
     path = f".github/workflows/run_scout_{scout['query'].replace(' ', '_')}.yml"
+
+    contents = repo.get_contents(path)
+    if contents:
+        return "File already exists"
 
     # Commit the file to the repository
     repo.create_file(path, f"Create action for scout {scout['query']}", workflow_content, branch="main")
 
+
+
+# In /run-scouts get all scouts that have that query and add results for all of them.
+
+# What if query containes unique characters? How to handle that?
 
 
 if __name__ == '__main__':
