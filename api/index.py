827d3512742753c0f87f12c7347e1425ed7a0f4b
Josh Waller <joshwaller101@hotmail.com>
2024-05-17 01:59:21 +0100

    chore: Add create_action endpoint to create GitHub Actions

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import os
-
+from github import Github
 
 
 
@@ -24,22 +24,83 @@ def home():
 
 
 
-@app.route('/run-scout', methods=['POST'])
-def run_scout():
-    email = request.json['email']
-    query = request.json['query']
+# @app.route('/run-scout', methods=['POST'])
+# def run_scout():
+#     email = request.json['email']
+#     query = request.json['query']
     
+#     user = users.find_one({'email': email})
+
+
+
+
+#     return "Scout ran successfully"
+
+
+@app.route('/create-action', methods=['POST'])
+def create_action():
+    email = request.json['owner']
+    country = request.json['country']
+    query = request.json['query']
+
+    # Get the user from the database
     user = users.find_one({'email': email})
+    
+    # Get the scout from the database
+    scout = scouts.find_one({'query': query, 'country': country})
+
+    if scout and user:
+        create_github_action(scout)
+        return "GitHub Action created successfully"
+    else:
+        return "User or scout not found"
 
 
 
 
-    return "Scout ran successfully"
 
 
 
 
 
+
+
+def create_github_action(scout):
+    token = os.environ.get('GITHUB_TOKEN')
+    repo_name = os.environ.get('GITHUB_REPO_NAME')
+    g = Github(token)
+    repo = g.get_user().get_repo(repo_name)
+
+    # Generate the GitHub Action workflow content
+    workflow_content = f"""
+    name: Run Scout {scout['query']}
+
+    on:
+    schedule:
+        - cron:  '0 * * * *'
+
+    jobs:
+    run-scout:
+        runs-on: ubuntu-latest
+        steps:
+        - name: Send POST request
+            uses: fjogeleit/http-request-action@master
+            with:
+            url: 'https://yourdomain.com/run-scout'
+            method: 'POST'
+            contentType: 'application/json'
+            data: '{{
+                "owner": "{scout['owner']}",
+                "query": "{scout['query']}",
+                "country": "{scout['country']}"
+            }}'
+    """
+
+    # Path where the GitHub Action will be saved
+    path = f".github/workflows/run_scout_{scout['query'].replace(' ', '_')}.yml"
+
+    # Commit the file to the repository
+    repo.create_file(path, f"Create action for scout {scout['query']}", workflow_content, branch="main")
 
 
 
