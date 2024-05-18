8633f06f0ac3b1e8ac9dc522798dfd82ea1540d1
Josh Waller <joshwaller101@hotmail.com>
2024-05-18 03:25:59 +0100

    add delete_action function

--- api/index.py
+++ api/index.py
@@ -57,9 +57,45 @@ def create_action():
 
 
 
+@app.route('/delete-action', methods=['POST'])
+def delete_action():
+    email = request.json['owner']
+    country = request.json['country']
+    query = request.json['query']
+
+    # Get the user from the database
+    user = users.find_one({'email': email})
+    
+    # Get the scout from the database
+    scout = scouts.find_one({'query': query, 'country': country})
+
+    if scout and user:
+        delete_github_action(scout)
+        return "GitHub Action deleted successfully"
+    else:
+        return "User or scout not found"
 
 
 
+
+def delete_github_action(scout):
+    token = os.environ.get('GITHUB_TOKEN')
+    repo_name = os.environ.get('GITHUB_REPO_NAME')
+    g = Github(token)
+    repo = g.get_user().get_repo(repo_name)
+
+    user = users.find_one({'email': scout['owner']})
+
+    safe_query = urllib.parse.quote(scout['query'].replace(' ', '_'), safe='')
+    path = f".github/workflows/run_scout_{safe_query}_{scout['country']}_{str(user['_id'])}.yml"
+
+    try:
+        contents = repo.get_contents(path)
+        repo.delete_file(contents.path, f"Delete action for scout {scout['query']}", contents.sha, branch="main")
+    except GithubException as e:
+        return 'File not found!'
+
+    return "GitHub Action deleted successfully!"
 
 
 
